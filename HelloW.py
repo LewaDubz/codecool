6d54bd089fe8aaae9e488b9e426d5f10b2a8a98e
LewaDubz <lestrider@gmail.com>
2016-10-31 23:00:32 +0100

    HelloWorld

---
+++ HelloW.py
@@ -0,0 +1,13 @@
+import sys
+    class Program():
+        def __init__(self):
+        self.name="World"
+     def NameCheck(self):
+        if(len(sys.argv) > 1):
+            self.name = sys.argv[1]
+    def PrintIt(self):
+        print("Hello " + str(self.name) + "!")
+
+app1 = Program()
+app1.NameCheck()
+app1.PrintIt()
